9ce9f8a592357a709415b2ad8d93052aae808321
Abubakarsidiq01 <bolakaleabubakar837@gmail.com>
2025-05-21 22:57:35 -0500

    1748

---
+++ 1748_Sum_Of_Unique_Elements.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+class Solution:
+    def sumOfUnique(self, nums: list[int]) -> int:
+        seen = defaultdict(int)
+        for num in nums:
+            seen[num] += 1
+        ans = 0
+        for key in seen:
+            if seen[key] == 1:
+                ans += key
+        return ans
+
+        
+sol = Solution()
+nums = [1,2,3,2]
+print(sol.sumOfUnique(nums))
