817333f1dc6c30ad1536037eb453616a85999d0c
Abubakarsidiq01 <bolakaleabubakar837@gmail.com>
2025-06-15 21:10:46 -0500

    344

---
+++ 344_Reverse_String.py
@@ -0,0 +1,17 @@
+class Solution:
+    def reverseString(self, s: list[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        left, right = 0, len(s)-1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+
+solution = Solution()
+solution.reverseString(["h","e","l","l","o"])
+
+s = ["h","e","l","l","o"]
+solution.reverseString(s)
+print(s)
