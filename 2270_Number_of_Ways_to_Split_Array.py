ed4075f7f89530fb7ca7e20b45d35133b1042aba
Abubakarsidiq01 <bolakaleabubakar837@gmail.com>
2025-06-19 22:38:17 -0500

    2270

---
+++ 2270_Number_of_Ways_to_Split_Array.py
@@ -0,0 +1,19 @@
+class Solution:
+    def waysToSplitArray(self, nums: list[int]) -> int:
+        a = sum(nums)
+        b = count = 0
+        for i in range(len(nums)-1):
+            b += nums[i]
+            if b >= (a-b):
+                count += 1
+        return count
+solution = Solution()
+print(solution.waysToSplitArray([10,4,-8,7]))
+
+"""
+count = 0
+for i in range(1, len(nums)):
+    if sum(nums[:i]) >= sum(nums[i:]):
+        count += 1
+return count
+"""
---
+++ practice.py
@@ -0,0 +1,27 @@
+def answer_queries(nums, queries, limit):
+    prefix = [nums[0]]
+    for i in range(1, len(nums)):
+        prefix.append(nums[i] + prefix[-1])
+    
+    ans = []
+    for x, y in queries:
+        curr = prefix[y] - prefix[x - 1]
+        ans.append(curr < limit)
+
+    return ans
+
+print(answer_queries([1, 6, 3, 2, 7, 2], [[0, 3], [3, 5], [2, 4]], 13))
+
+lst = [3, 6, 2, 8, 1, 4, 1, 5]
+ans = [lst[0]]
+for i in range(1, len(lst)):
+    ans.append(lst[i] + ans[-1])
+
+for i in range(3,7):
+    print(ans[i])
+
+print(lst[:1])
+print(lst[1:])
+
+
+
