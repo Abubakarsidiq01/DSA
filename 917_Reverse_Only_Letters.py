81effbe7f6beca061205fa66fc900f7bc1c462aa
Abubakarsidiq01 <bolakaleabubakar837@gmail.com>
2025-06-22 22:39:30 -0500

    1496

---
+++ 917_Reverse_Only_Letters.py
@@ -0,0 +1,18 @@
+class Solution:
+    def reverseOnlyLetters(self, s: str) -> str:
+        s = list(s)
+        left = 0
+        right = len(s) -1
+        while left < right:
+            if s[left].isalpha() and s[right].isalpha():
+                s[left], s[right] = s[right], s[left]
+                right -= 1
+                left += 1
+            elif not s[left].isalpha():
+                left += 1
+            elif not s[right].isalpha():
+                right -= 1
+        return "".join(s)
+
+solution = Solution()
+print(solution.reverseOnlyLetters("ab-cd"))
