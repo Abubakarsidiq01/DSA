f4268ea18cd4bd3fe7a7f5f94f4b496e55c0456b
Abubakarsidiq01 <bolakaleabubakar837@gmail.com>
2025-07-05 18:04:53 -0500

    new 205

--- 205_Isomorphic_Strings.py
+++ 205_Isomorphic_Strings.py
@@ -1,13 +1,28 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        dic1 = {} # dic of s to t
-        dic2 = {} # dic of t to s
-        for i in range (len(s)):
-            if (s[i] in dic1 and dic1[s[i]] != t[i]) or (t[i] in dic2 and dic2[t[i]] != s[i]):
-                return False
-            dic1[s[i]] = t[i]
-            dic2[t[i]] = s[i]
+        dict={}
+        if(len(s)!=len(t)):
+            return False
+
+        for i in range(len(s)):
+            if s[i] in dict.keys():
+                if dict[s[i]] != t[i]:
+                    return False
+            else:
+                if t[i] in dict.values(): # Key(s) not in dict but the value pair to it is already used.
+                    return False
+                dict[s[i]]=t[i]
         return True
+"""
+dic1 = {} # dic of s to t
+dic2 = {} # dic of t to s
+for i in range (len(s)):
+    if (s[i] in dic1 and dic1[s[i]] != t[i]) or (t[i] in dic2 and dic2[t[i]] != s[i]):
+        return False
+    dic1[s[i]] = t[i]
+    dic2[t[i]] = s[i]
+return True
+"""
 
 sol = Solution()
 print(sol.isIsomorphic("egg", "add"))
