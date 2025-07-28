d40b806b3d114926af4b6624344d5e37fa7b8c19
Abubakarsidiq01 <bolakaleabubakar837@gmail.com>
2025-07-28 14:16:17 -0500

    2126

---
+++ 2126. Destroying Asteroids.py
@@ -0,0 +1,14 @@
+class Solution:
+    def asteroidsDestroyed(self, mass: int, asteroids: List[int]) -> bool:
+        asteroids.sort()
+        for asteroid in asteroids:
+            if asteroid > mass:
+                return False
+            mass += asteroid
+        
+        return True
+solution = Solution()
+print(solution.asteroidsDestroyed(10, [3, 9, 19, 5, 21]))
+print(solution.asteroidsDestroyed(5, [4, 9, 19, 5, 21]))
+print(solution.asteroidsDestroyed(10, [3, 9, 19, 5, 21]))
+print(solution.asteroidsDestroyed(10, [3, 9, 19, 5, 21]))
