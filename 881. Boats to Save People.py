585f144d8ac8a26995febcd9b3ddcc2fd5e774da
Abubakarsidiq01 <bolakaleabubakar837@gmail.com>
2025-08-03 22:16:57 -0500

    new

--- 881. Boats to Save People.py
+++ 881. Boats to Save People.py
@@ -1,15 +1,18 @@
 class Solution:
     def numRescueBoats(self, people: list[int], limit: int) -> int:
-        ans = 0
-        i = 0
-        j = len(people) - 1
         people.sort()
-        
-        while i <= j:
-            if people[i] + people[j] <= limit:
-                i += 1
-
-            j -= 1
-            ans += 1
-        
-        return ans
+        left = 0
+        right = len(people) - 1
+        count = 0
+        while left <= right:
+            if people[left] + people[right] <= limit:
+                left += 1
+            right -= 1
+            count += 1
+        return count
+s = Solution()
+print(s.numRescueBoats([1, 2], 3))
+print(s.numRescueBoats([3, 5, 3, 4], 5))
+print(s.numRescueBoats([3, 2, 2, 1], 3))
+print(s.numRescueBoats([3, 5, 3, 4], 5))
+print(s.numRescueBoats([3, 5, 3, 4], 5))
