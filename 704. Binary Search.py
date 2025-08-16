e0de05475f8e4ecfff5e189ed1fa30977834fe26
Abubakarsidiq01 <bolakaleabubakar837@gmail.com>
2025-08-15 19:40:28 -0500

    704

---
+++ 704. Binary Search.py
@@ -0,0 +1,15 @@
+class Solution:
+    def search(self, nums: list[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return -1
+solution = Solution()
+print(solution.search([-1,0,3,5,9,12], 9))
