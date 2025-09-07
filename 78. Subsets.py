eec08acc43be1e530a144e09a10f853fab3db507
Abubakarsidiq01 <bolakaleabubakar837@gmail.com>
2025-09-06 21:17:53 -0500

    78

---
+++ 78. Subsets.py
@@ -0,0 +1,17 @@
+class Solution:
+    def subsets(self, nums: list[int]) -> list[list[int]]:
+        ans = []
+        def backtrack(curr, i):
+            if i > len(nums):
+                return
+            ans.append(curr[:])
+            for j in range(i, len(nums)):
+                curr.append(nums[j])
+                backtrack(curr, j + 1)
+                curr.pop()
+
+        backtrack([], 0)
+        return ans
+sol = Solution()
+print(sol.subsets([1,2,3]))
+print(sol.subsets([0]))
